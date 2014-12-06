407eb3690768a5dc486c721f70f741014b0a806c
Nick Jennings <nick@silverbucket.net>
2014-12-06 15:18:07 +0100

    added build file

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp       = require('gulp'),
+    fs         = require('fs'),
+    browserify = require('gulp-browserify'),
+    uglify     = require('gulp-uglify'),
+    rename     = require('gulp-rename'),
+    pkg        = require('./package.json');
+
+gulp.task('default', function () {
+
+  gulp.src('array-keys.js')
+      .pipe(browserify({ standalone: 'ArrayKeys' }))
+      .pipe(gulp.dest('dist/'))
+      .pipe(uglify())
+      .pipe(rename('array-keys.min.js'))
+      .pipe(gulp.dest('dist/'));
+});
