5630dd9fd8dbe8238790ef2a9734b3b7734be3d3
Nick Jennings <nick@silverbucket.net>
2014-12-06 15:16:52 +0100

    update tests for browserify

--- test/basics-suite.js
+++ test/basics-suite.js
@@ -1,98 +1,125 @@
+function getTests() {
+  return [
+    {
+      desc: 'ensure amd module is loaded correctly',
+      run: function (env, test) {
+        if (typeof amdMod !== 'undefined') {
+          test.assertTypeAnd(amdMod, 'function');
+          var amdmod = new amdMod();
+          test.assertType(amdmod.addRecord, 'function');
+        } else{
+          test.done();
+        }
+      }
+    },
+
+    {
+      desc: '# getRecordIFExists  - with no params returns undefined',
+      run: function (env, test) {
+        test.assert(env.mod.getRecord('thingy'), undefined);
+      }
+    },
+
+    {
+      desc: '# addRecord 1',
+      run: function (env, test) {
+        test.assert(env.mod.addRecord({id:'thingy1'}), true);
+      }
+    },
+
+    {
+      desc: '# getRecordIFExists',
+      run: function (env, test) {
+        test.assert(env.mod.getRecord('thingy1'), {id:'thingy1'});
+      }
+    },
+
+    {
+      desc: '# addRecord with no identifier',
+      run: function (env, test) {
+        test.throws(env.mod.addRecord, Error, 'caught thrown exception');
+      }
+    },
+
+    {
+      desc: '# addRecord 2',
+      run: function (env, test) {
+        test.assert(env.mod.addRecord({id:'thingy2'}), true);
+      }
+    },
+
+    {
+      desc: '# addRecord 3',
+      run: function (env, test) {
+        test.assert(env.mod.addRecord({id:'thingy3'}), true);
+      }
+    },
+
+    {
+      desc: '# getCount (3)',
+      run: function (env, test) {
+        test.assert(env.mod.getCount(), 3);
+      }
+    },
+
+    {
+      desc: '# getIndexes',
+      run: function (env, test) {
+        test.assert(env.mod.getIdentifiers(), ['thingy3', 'thingy2', 'thingy1']);
+      }
+    },
+
+    {
+      desc: '# removeRecord 2',
+      run: function (env, test) {
+        test.assert(env.mod.removeRecord('thingy2'), true);
+      }
+    },
+
+    {
+      desc: '# getCount (2)',
+      run: function (env, test) {
+        test.assert(env.mod.getCount(), 2);
+      }
+    },
+  ];
+}
+
+
 if (typeof define !== 'function') {
   var define = require('amdefine')(module);
 }
-define(['require', './../array-keys.js'], function (require, ArrayKeysAMD) {
-  var suites = [];
-
-  suites.push({
+define(['require'], function (require) {
+  return [{
     desc: "basic tests",
     setup: function (env, test) {
-      env.ArrayKeys = require('./../array-keys.js');
-      test.assertTypeAnd(env.ArrayKeys, 'function');
-      env.ak = new env.ArrayKeys();
-      test.assertType(env.ak.addRecord, 'function');
+      var Mod = require('./../array-keys');
+      test.assertTypeAnd(Mod, 'function');
+      env.mod = new Mod();
+      test.assertType(env.mod, 'object');
     },
-    tests: [
-      {
-        desc: 'ensure amd module is loaded correctly',
-        run: function (env, test) {
-          test.assertTypeAnd(ArrayKeysAMD, 'function');
-          var amdak = new ArrayKeysAMD();
-          test.assertType(amdak.addRecord, 'function');
-        }
-      },
-
-      {
-        desc: '# getRecordIFExists  - with no params returns undefined',
-        run: function (env, test) {
-          test.assert(env.ak.getRecord('thingy'), undefined);
-        }
-      },
-
-      {
-        desc: '# addRecord 1',
-        run: function (env, test) {
-          test.assert(env.ak.addRecord({id:'thingy1'}), true);
-        }
-      },
-
-      {
-        desc: '# getRecordIFExists',
-        run: function (env, test) {
-          test.assert(env.ak.getRecord('thingy1'), {id:'thingy1'});
-        }
-      },
-
-      {
-        desc: '# addRecord with no identifier',
-        run: function (env, test) {
-          test.throws(env.ak.addRecord, Error, 'caught thrown exception');
-        }
-      },
-
-      {
-        desc: '# addRecord 2',
-        run: function (env, test) {
-          test.assert(env.ak.addRecord({id:'thingy2'}), true);
-        }
-      },
-
-      {
-        desc: '# addRecord 3',
-        run: function (env, test) {
-          test.assert(env.ak.addRecord({id:'thingy3'}), true);
-        }
-      },
-
-      {
-        desc: '# getCount (3)',
-        run: function (env, test) {
-          test.assert(env.ak.getCount(), 3);
-        }
-      },
-
-      {
-        desc: '# getIndexes',
-        run: function (env, test) {
-          test.assert(env.ak.getIdentifiers(), ['thingy3', 'thingy2', 'thingy1']);
-        }
-      },
-
-      {
-        desc: '# removeRecord 2',
-        run: function (env, test) {
-          test.assert(env.ak.removeRecord('thingy2'), true);
-        }
-      },
-
-      {
-        desc: '# getCount (2)',
-        run: function (env, test) {
-          test.assert(env.ak.getCount(), 2);
-        }
-      },
-    ]
-  });
-
-  return suites;
+    tests: getTests(),
+  },
+  {
+    desc: "basic tests (browserify)",
+    setup: function (env, test) {
+      var Mod = require('./../dist/array-keys.js');
+console.log('mod: ', Mod);
+      test.assertTypeAnd(Mod, 'function');
+      env.mod = new Mod();
+      test.assertType(env.mod, 'object');
+    },
+    tests: getTests(),
+  },
+  {
+    desc: "basic tests (browserify minified)",
+    setup: function (env, test) {
+      var Mod = require('./../dist/array-keys.min.js');
+console.log('mod: ', Mod);
+      test.assertTypeAnd(Mod, 'function');
+      env.mod = new Mod();
+      test.assertType(env.mod, 'object');
+    },
+    tests: getTests(),
+  }];
 });
