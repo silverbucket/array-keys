773633e54eb962ffcf6b1a7f7f5e568b9ac03301
Nick Jennings <nick@silverbucket.net>
2014-12-11 15:21:09 +0100

    build files

--- browser/array-keys.js
+++ browser/array-keys.js
@@ -1,7 +1,7 @@
 !function(e){if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.ArrayKeys=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 /*!
  * array-keys
- *   version 1.2.1
+ *   version 1.2.2
  *   http://github.com/silverbucket/array-keys
  *
  * Developed and Maintained by:
@@ -42,7 +42,7 @@ ArrayKeys.prototype.getRecord = function (id) {
 };
 
 ArrayKeys.prototype.exists = function (id) {
-  if (this.idx.getIndex(id) >= 0) {
+  if (this.getIndex(id) >= 0) {
     return true;
   } else {
     return false;
