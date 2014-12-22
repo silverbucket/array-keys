db3ea45a02ee3277cf10292059318358d740e9d6
Nick Jennings <nick@silverbucket.net>
2014-12-22 20:13:11 +0100

    update build

--- browser/array-keys.js
+++ browser/array-keys.js
@@ -1,7 +1,7 @@
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.ArrayKeys=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /*!
  * array-keys
- *   version 1.2.9
+ *   version 2.0.0
  *   http://github.com/silverbucket/array-keys
  *
  * Developed and Maintained by:
@@ -17,25 +17,37 @@
  *
  */
 
+var EventEmitter = require('event-emitter');
+
 function ArrayKeys(p) {
   if (typeof p !== 'object') { p = {}; }
-  this.identifier = p.identifier || 'id';
-  this.store = [];
-  this.idx = []; // array of identifier strings for quick lookup
+  this._identifier = p.identifier || 'id';
+  this._store = [];
+  this._idx = []; // array of identifier strings for quick lookup
+  if (p.emitEvents) {
+    this.emitEvents = true;
+    this.events = new EventEmitter();
+  }
 }
+
+ArrayKeys.prototype.emitEvent = function (event, data, dontEmit) {
+  if ((this.emitEvents) && (! dontEmit)) {
+    this.events.emit(event, data);
+  }
+};
 
 ArrayKeys.prototype.getIdentifiers = function () {
   var ids = [];
-  for (var i = this.store.length - 1; i >= 0; i = i - 1) {
-    ids[ids.length] = this.store[i][this.identifier];
+  for (var i = this._store.length - 1; i >= 0; i = i - 1) {
+    ids[ids.length] = this._store[i][this._identifier];
   }
   return ids;
 };
 
 ArrayKeys.prototype.getRecord = function (id) {
-  for (var i = this.store.length - 1; i >= 0; i = i - 1) {
-    if (this.store[i][this.identifier] === id) {
-      return this.store[i];
+  for (var i = this._store.length - 1; i >= 0; i = i - 1) {
+    if (this._store[i][this._identifier] === id) {
+      return this._store[i];
     }
   }
   return undefined;
@@ -51,8 +63,8 @@ ArrayKeys.prototype.exists = function (id) {
 
 // faster than using indexOf
 ArrayKeys.prototype.getIndex = function (id) {
-  for (var i = this.idx.length - 1; i >= 0; i = i - 1) {
-    if (this.idx[i] === id) {
+  for (var i = this._idx.length - 1; i >= 0; i = i - 1) {
+    if (this._idx[i] === id) {
       return i;
     }
   }
@@ -62,18 +74,25 @@ ArrayKeys.prototype.getIndex = function (id) {
 ArrayKeys.prototype.addRecord = function (record) {
   if (typeof record !== 'object') {
     throw new Error('cannot add non-object records.');
-  } else if (!record[this.identifier]) {
-    throw new Error('cannot add a record with no `' + this.identifier +
+  } else if (!record[this._identifier]) {
+    throw new Error('cannot add a record with no `' + this._identifier +
                     '` property specified.');
   }
 
-  this.removeRecord(record[this.identifier]);
-  this.idx[this.idx.length] = record[this.identifier];
-  this.store[this.store.length] = record;
+  var removed = this.removeRecord(record[this._identifier], true);
+  this._idx[this._idx.length] = record[this._identifier];
+  this._store[this._store.length] = record;
+  setTimeout(function () {
+    if (removed) {
+      setTimeout(this.emitEvent.bind(this, 'update', record), 0);
+    } else {
+      setTimeout(this.emitEvent.bind(this, 'add', record), 0);
+    }
+  }.bind(this), 0);
   return true;
 };
 
-ArrayKeys.prototype.removeRecord = function (id) {
+ArrayKeys.prototype.removeRecord = function (id, dontEmit) {
   var idx  = this.getIndex(id);
   if (idx < 0) {
     return false;
@@ -81,18 +100,20 @@ ArrayKeys.prototype.removeRecord = function (id) {
 
   // start looking for the record at the same point as the idx entry
   for (var i = idx; i >= 0; i = i - 1) {
-    if (this.store[i][this.identifier] === id) {
-      this.store.splice(i, 1);
-      this.idx.splice(idx, 1);
+    if (this._store[i][this._identifier] === id) {
+      this._store.splice(i, 1);
+      this._idx.splice(idx, 1);
+      setTimeout(this.emitEvent.bind(this, 'remove', id, dontEmit), 0);
       return true;
     }
   }
 
   // if it was not found, start at the end and break at the idx number
-  for (var n = this.store.length - 1; n !== idx; n = n - 1) {
-    if (this.store[n][this.identifier] === id) {
-      this.store.splice(n, 1);
-      this.idx.splice(idx, 1);
+  for (var n = this._store.length - 1; n !== idx; n = n - 1) {
+    if (this._store[n][this._identifier] === id) {
+      this._store.splice(n, 1);
+      this._idx.splice(idx, 1);
+      setTimeout(this.emitEvent.bind(this, 'remove', id, dontEmit), 0);
       return true;
     }
   }
@@ -105,9 +126,9 @@ ArrayKeys.prototype.forEachRecord = function (cb) {
   var finished = function () {};
 
   setTimeout(function () {
-    for (var i = self.store.length - 1; i >= 0; i = i - 1) {
+    for (var i = self._store.length - 1; i >= 0; i = i - 1) {
       count += 1;
-      setTimeout(cb(self.store[i]), 0);
+      setTimeout(cb(self._store[i]), 0);
     }
     setTimeout(finished(count), 0);
   }, 0);
@@ -120,10 +141,350 @@ ArrayKeys.prototype.forEachRecord = function (cb) {
 };
 
 ArrayKeys.prototype.getCount = function () {
-  return this.store.length;
+  return this._store.length;
 };
 
 module.exports = ArrayKeys;
+
+},{"event-emitter":2}],2:[function(require,module,exports){
+'use strict';
+
+var d        = require('d')
+  , callable = require('es5-ext/object/valid-callable')
+
+  , apply = Function.prototype.apply, call = Function.prototype.call
+  , create = Object.create, defineProperty = Object.defineProperty
+  , defineProperties = Object.defineProperties
+  , hasOwnProperty = Object.prototype.hasOwnProperty
+  , descriptor = { configurable: true, enumerable: false, writable: true }
+
+  , on, once, off, emit, methods, descriptors, base;
+
+on = function (type, listener) {
+	var data;
+
+	callable(listener);
+
+	if (!hasOwnProperty.call(this, '__ee__')) {
+		data = descriptor.value = create(null);
+		defineProperty(this, '__ee__', descriptor);
+		descriptor.value = null;
+	} else {
+		data = this.__ee__;
+	}
+	if (!data[type]) data[type] = listener;
+	else if (typeof data[type] === 'object') data[type].push(listener);
+	else data[type] = [data[type], listener];
+
+	return this;
+};
+
+once = function (type, listener) {
+	var once, self;
+
+	callable(listener);
+	self = this;
+	on.call(this, type, once = function () {
+		off.call(self, type, once);
+		apply.call(listener, this, arguments);
+	});
+
+	once.__eeOnceListener__ = listener;
+	return this;
+};
+
+off = function (type, listener) {
+	var data, listeners, candidate, i;
+
+	callable(listener);
+
+	if (!hasOwnProperty.call(this, '__ee__')) return this;
+	data = this.__ee__;
+	if (!data[type]) return this;
+	listeners = data[type];
+
+	if (typeof listeners === 'object') {
+		for (i = 0; (candidate = listeners[i]); ++i) {
+			if ((candidate === listener) ||
+					(candidate.__eeOnceListener__ === listener)) {
+				if (listeners.length === 2) data[type] = listeners[i ? 0 : 1];
+				else listeners.splice(i, 1);
+			}
+		}
+	} else {
+		if ((listeners === listener) ||
+				(listeners.__eeOnceListener__ === listener)) {
+			delete data[type];
+		}
+	}
+
+	return this;
+};
+
+emit = function (type) {
+	var i, l, listener, listeners, args;
+
+	if (!hasOwnProperty.call(this, '__ee__')) return;
+	listeners = this.__ee__[type];
+	if (!listeners) return;
+
+	if (typeof listeners === 'object') {
+		l = arguments.length;
+		args = new Array(l - 1);
+		for (i = 1; i < l; ++i) args[i - 1] = arguments[i];
+
+		listeners = listeners.slice();
+		for (i = 0; (listener = listeners[i]); ++i) {
+			apply.call(listener, this, args);
+		}
+	} else {
+		switch (arguments.length) {
+		case 1:
+			call.call(listeners, this);
+			break;
+		case 2:
+			call.call(listeners, this, arguments[1]);
+			break;
+		case 3:
+			call.call(listeners, this, arguments[1], arguments[2]);
+			break;
+		default:
+			l = arguments.length;
+			args = new Array(l - 1);
+			for (i = 1; i < l; ++i) {
+				args[i - 1] = arguments[i];
+			}
+			apply.call(listeners, this, args);
+		}
+	}
+};
+
+methods = {
+	on: on,
+	once: once,
+	off: off,
+	emit: emit
+};
+
+descriptors = {
+	on: d(on),
+	once: d(once),
+	off: d(off),
+	emit: d(emit)
+};
+
+base = defineProperties({}, descriptors);
+
+module.exports = exports = function (o) {
+	return (o == null) ? create(base) : defineProperties(Object(o), descriptors);
+};
+exports.methods = methods;
+
+},{"d":3,"es5-ext/object/valid-callable":12}],3:[function(require,module,exports){
+'use strict';
+
+var assign        = require('es5-ext/object/assign')
+  , normalizeOpts = require('es5-ext/object/normalize-options')
+  , isCallable    = require('es5-ext/object/is-callable')
+  , contains      = require('es5-ext/string/#/contains')
+
+  , d;
+
+d = module.exports = function (dscr, value/*, options*/) {
+	var c, e, w, options, desc;
+	if ((arguments.length < 2) || (typeof dscr !== 'string')) {
+		options = value;
+		value = dscr;
+		dscr = null;
+	} else {
+		options = arguments[2];
+	}
+	if (dscr == null) {
+		c = w = true;
+		e = false;
+	} else {
+		c = contains.call(dscr, 'c');
+		e = contains.call(dscr, 'e');
+		w = contains.call(dscr, 'w');
+	}
+
+	desc = { value: value, configurable: c, enumerable: e, writable: w };
+	return !options ? desc : assign(normalizeOpts(options), desc);
+};
+
+d.gs = function (dscr, get, set/*, options*/) {
+	var c, e, options, desc;
+	if (typeof dscr !== 'string') {
+		options = set;
+		set = get;
+		get = dscr;
+		dscr = null;
+	} else {
+		options = arguments[3];
+	}
+	if (get == null) {
+		get = undefined;
+	} else if (!isCallable(get)) {
+		options = get;
+		get = set = undefined;
+	} else if (set == null) {
+		set = undefined;
+	} else if (!isCallable(set)) {
+		options = set;
+		set = undefined;
+	}
+	if (dscr == null) {
+		c = true;
+		e = false;
+	} else {
+		c = contains.call(dscr, 'c');
+		e = contains.call(dscr, 'e');
+	}
+
+	desc = { get: get, set: set, configurable: c, enumerable: e };
+	return !options ? desc : assign(normalizeOpts(options), desc);
+};
+
+},{"es5-ext/object/assign":4,"es5-ext/object/is-callable":7,"es5-ext/object/normalize-options":11,"es5-ext/string/#/contains":14}],4:[function(require,module,exports){
+'use strict';
+
+module.exports = require('./is-implemented')()
+	? Object.assign
+	: require('./shim');
+
+},{"./is-implemented":5,"./shim":6}],5:[function(require,module,exports){
+'use strict';
+
+module.exports = function () {
+	var assign = Object.assign, obj;
+	if (typeof assign !== 'function') return false;
+	obj = { foo: 'raz' };
+	assign(obj, { bar: 'dwa' }, { trzy: 'trzy' });
+	return (obj.foo + obj.bar + obj.trzy) === 'razdwatrzy';
+};
+
+},{}],6:[function(require,module,exports){
+'use strict';
+
+var keys  = require('../keys')
+  , value = require('../valid-value')
+
+  , max = Math.max;
+
+module.exports = function (dest, src/*, …srcn*/) {
+	var error, i, l = max(arguments.length, 2), assign;
+	dest = Object(value(dest));
+	assign = function (key) {
+		try { dest[key] = src[key]; } catch (e) {
+			if (!error) error = e;
+		}
+	};
+	for (i = 1; i < l; ++i) {
+		src = arguments[i];
+		keys(src).forEach(assign);
+	}
+	if (error !== undefined) throw error;
+	return dest;
+};
+
+},{"../keys":8,"../valid-value":13}],7:[function(require,module,exports){
+// Deprecated
+
+'use strict';
+
+module.exports = function (obj) { return typeof obj === 'function'; };
+
+},{}],8:[function(require,module,exports){
+'use strict';
+
+module.exports = require('./is-implemented')()
+	? Object.keys
+	: require('./shim');
+
+},{"./is-implemented":9,"./shim":10}],9:[function(require,module,exports){
+'use strict';
+
+module.exports = function () {
+	try {
+		Object.keys('primitive');
+		return true;
+	} catch (e) { return false; }
+};
+
+},{}],10:[function(require,module,exports){
+'use strict';
+
+var keys = Object.keys;
+
+module.exports = function (object) {
+	return keys(object == null ? object : Object(object));
+};
+
+},{}],11:[function(require,module,exports){
+'use strict';
+
+var assign = require('./assign')
+
+  , forEach = Array.prototype.forEach
+  , create = Object.create, getPrototypeOf = Object.getPrototypeOf
+
+  , process;
+
+process = function (src, obj) {
+	var proto = getPrototypeOf(src);
+	return assign(proto ? process(proto, obj) : obj, src);
+};
+
+module.exports = function (options/*, …options*/) {
+	var result = create(null);
+	forEach.call(arguments, function (options) {
+		if (options == null) return;
+		process(Object(options), result);
+	});
+	return result;
+};
+
+},{"./assign":4}],12:[function(require,module,exports){
+'use strict';
+
+module.exports = function (fn) {
+	if (typeof fn !== 'function') throw new TypeError(fn + " is not a function");
+	return fn;
+};
+
+},{}],13:[function(require,module,exports){
+'use strict';
+
+module.exports = function (value) {
+	if (value == null) throw new TypeError("Cannot use null or undefined");
+	return value;
+};
+
+},{}],14:[function(require,module,exports){
+'use strict';
+
+module.exports = require('./is-implemented')()
+	? String.prototype.contains
+	: require('./shim');
+
+},{"./is-implemented":15,"./shim":16}],15:[function(require,module,exports){
+'use strict';
+
+var str = 'razdwatrzy';
+
+module.exports = function () {
+	if (typeof str.contains !== 'function') return false;
+	return ((str.contains('dwa') === true) && (str.contains('foo') === false));
+};
+
+},{}],16:[function(require,module,exports){
+'use strict';
+
+var indexOf = String.prototype.indexOf;
+
+module.exports = function (searchString/*, position*/) {
+	return indexOf.call(this, searchString, arguments[1]) > -1;
+};
 
 },{}]},{},[1])(1)
 });
