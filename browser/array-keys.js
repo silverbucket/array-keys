9f56ec90ff9d7fa95912fd5597cad0f0df6d74dd
Nick Jennings <nick@silverbucket.net>
2015-06-08 13:55:11 +0200

    build files

--- browser/array-keys.js
+++ browser/array-keys.js
@@ -1,11 +1,11 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.ArrayKeys = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /*!
  * array-keys
- *   version 2.0.0
+ *   version 2.1.0
  *   http://github.com/silverbucket/array-keys
  *
  * Developed and Maintained by:
- *   Nick Jennings <nick@silverbucket.net> copyright 2014
+ *   Nick Jennings <nick@silverbucket.net> copyright 2015
  *
  * array-keys is released under the MIT license (see LICENSE).
  *
@@ -144,6 +144,13 @@ ArrayKeys.prototype.forEachRecord = function (cb) {
 
 ArrayKeys.prototype.getCount = function () {
   return this._store.length;
+};
+
+ArrayKeys.prototype.removeAll = function () {
+  for (var i = this._store.length - 1; i >= 0; i = i - 1) {
+    delete this._store[i];
+  }
+  this._store = [];
 };
 
 module.exports = ArrayKeys;
