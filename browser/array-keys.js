ee2bf0f9ef4764ecbe1089d2390732cef354a7e0
Nick Jennings <nick@silverbucket.net>
2017-01-12 15:22:56 +0700

    update builds

--- browser/array-keys.js
+++ browser/array-keys.js
@@ -1,7 +1,7 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.ArrayKeys = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /*!
  * array-keys
- *   version 2.3.0
+ *   version 2.3.1
  *   http://github.com/silverbucket/array-keys
  *
  * Developed and Maintained by:
@@ -112,7 +112,7 @@ ArrayKeys.prototype.removeRecord = function (id, dontEmit) {
 
   // if it was not found, start at the end and break at the idx number
   for (var n = this._store.length - 1; n >= idx; n = n - 1) {
-    if ((this._store[i]) && (this._store[n][this._identifier] === id)) {
+    if ((this._store[n]) && (this._store[n][this._identifier] === id)) {
       this._store.splice(n, 1);
       this._idx.splice(idx, 1);
       setTimeout(this.emitEvent.bind(this, 'remove', id, dontEmit), 0);
